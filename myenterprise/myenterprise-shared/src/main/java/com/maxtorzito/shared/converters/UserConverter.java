b2e026bb9254e64a13333534ca3c908d7058d58e
José Luis Cetina <maxtorzito@gmail.com>
2013-02-15 10:40:03 -0800

    converter fixed

--- myenterprise/myenterprise-shared/src/main/java/com/maxtorzito/shared/converters/UserConverter.java
+++ myenterprise/myenterprise-shared/src/main/java/com/maxtorzito/shared/converters/UserConverter.java
@@ -1,6 +1,5 @@
 package com.maxtorzito.shared.converters;
 
-
 import com.maxtorzito.shared.model.User;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -9,16 +8,23 @@ import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
 @FacesConverter(value = "userConverter")
-public class UserConverter implements Converter{
+public class UserConverter implements Converter {
 
     @Override
     public Object getAsObject(FacesContext fc, UIComponent uic, String userName) throws ConverterException {
-        System.out.println("username:"+userName);
-        return new User(userName);
+        System.out.println("username:" + userName);
+        if (userName != null) {
+            return new User(userName);
+        }
+        return null;
     }
 
     @Override
     public String getAsString(FacesContext fc, UIComponent uic, Object o) throws ConverterException {
-        return ((User)o).getUserName();
-    }   
+        if (o != null && o instanceof User) {
+            User user = (User) o;
+            return user.getUserName();
+        }
+        return null;
+    }
 }
